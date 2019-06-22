b0f1b129dc889fd85ed343f7327ffe91c8d4ffae
unknown <dylanross620@gmail.com>
2019-06-21 21:25:32 -0400

    Added dialogs to ask user if they are a host and ip to connect to if necessary

--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
+import javax.swing.JOptionPane;
+
 public class Main {
+
+    private static boolean isHost = false;
+    private static String ip = "";
+
     public static void main(String[] args) {
-        RoomWindow win = new RoomWindow();
+        int hostChoice = JOptionPane.showOptionDialog(null, "Are you a host or a client?", "Encryption Chat", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[]{"Host", "Client"}, "Host");
+        isHost = hostChoice == 0;
+        System.out.println(isHost);
+
+        if (!isHost)
+            ip = JOptionPane.showInputDialog(null, "Enter IP to connect to");
+
+        if (!isHost) {
+            try {
+                if (ip.equals(""))
+                    return;
+            }
+            catch (NullPointerException e) {
+                return;
+            }
+        }
+
+        new RoomWindow();
     }
 
     public static void sendMessage(String message) {
